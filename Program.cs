3505152efd51a73e3e0a1f426a27a5c0f3438a8f
huseyinkarpuzcu1 <huseyinkarpuzcu371@gmail.com>
2022-07-13 10:36:44 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace kamp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string kategoriEtiketi = "Kategori";
+            Console.WriteLine(kategoriEtiketi);
+
+            bool giris=true;
+
+            if (giris==true)
+            {
+                Console.WriteLine("Giriş Yapıldı");
+            }
+            else if (true)
+            {
+                
+            }
+            else
+            {
+                Console.WriteLine("Giriş Yapılmadı");
+            }
+        }
+    }
+}
